8edfc1517f6d063bfbe5be68efe6b8cea21bbc9b
Rezcon <rezcon@sdsu.edu>
2019-09-09 07:39:58 -0700

    Update simdd2import.py

--- simdd2import.py
+++ simdd2import.py
@@ -12,7 +12,7 @@ passowrd = ""
 
 def updateStudentInfo(redID, enrollmentStatus):
 	# Gets the student data with the specified RedID.
-	request = urllib2.Request("https://oncampusliving.sdsu.edu/StarRezREST/services/select/entry.xml?id1=" + redID)
+	request = urllib2.Request("https://......../select/entry.xml?id1=" + redID)
 	base64string = base64.encodestring('%s:%s' % (username, passowrd)).replace('\n', '')
 	request.add_header("Authorization", "Basic %s" % base64string)
 	try:
@@ -32,7 +32,7 @@ def updateStudentInfo(redID, enrollmentStatus):
 		root = ET.fromstring(response)
 		entryID = root[0][0].text
 		# Gets the custom field data unique to the student's enrollment status field.
-		request = urllib2.Request("https://oncampusliving.sdsu.edu/StarRezREST/services/select/entrycustomfield.xml?entryid=" + entryID + "&customfielddefinitionid=" + enrollmentStatusFieldID)
+		request = urllib2.Request("https://......./services/select/entrycustomfield.xml?entryid=" + entryID + "&customfielddefinitionid=" + enrollmentStatusFieldID)
 		base64string = base64.encodestring('%s:%s' % (username, passowrd)).replace('\n', '')
 		request.add_header("Authorization", "Basic %s" % base64string)
 		try:
@@ -52,7 +52,7 @@ def updateStudentInfo(redID, enrollmentStatus):
 			root = ET.fromstring(response)
 			fieldID = root[0][0].text
 			# Updates the student's enrollment status with the data from the file.
-			request = urllib2.Request("https://oncampusliving.sdsu.edu/StarRezREST/services/update/entrycustomfield/" + fieldID)
+			request = urllib2.Request("https://....../services/update/entrycustomfield/" + fieldID)
 			data = urllib.urlencode({'valuestring' : enrollmentStatus})
 			base64string = base64.encodestring('%s:%s' % (username, passowrd)).replace('\n', '')
 			request.add_header("Authorization", "Basic %s" % base64string)
